8d98cd84498e44b8ccc16505302a192be611ca28
und3rdg <und3rdg@gmail.com>
2020-01-05 21:22:30 +0000

    useSelector and useDispatch from redux hooks looks nice, but have some warnings in documentation.

--- redux_with_react/src/components/HooksCakeContainer.js
+++ redux_with_react/src/components/HooksCakeContainer.js
@@ -1,33 +1,25 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { buy_cake, delivery_cake } from '../redux'
 
-function CakeContainer(props) {
+function HooksCakeContainer(props) {
+    const num_of_cakes = useSelector(state => state.num_of_cakes)
+    const dispatch = useDispatch()
+
+    const handler_buy_cake      = () => dispatch(buy_cake(2))
+    const handler_delivery_cake = () => dispatch(delivery_cake(10))
+
     return (
         <div>
-            <h2>Number of hook cakes: {props.num_of_cakes}</h2>
+            <h2>Number of hook cakes: {num_of_cakes}</h2>
             <button
-                onClick={props.buy_cake}
+                onClick={handler_buy_cake}
             >Buy hook cake</button>
             <button
-                onClick={props.delivery_cake}
+                onClick={handler_delivery_cake}
             >Delivery hook cakes</button>
         </div>
     )
 }
 
-// SELECTORS
-const map_state_to_props = state => ({
-    num_of_cakes: state.num_of_cakes
-})
-
-const map_dispatch_to_props = dispatch => ({
-    buy_cake: () => dispatch(buy_cake(2)),
-    delivery_cake: () => dispatch(delivery_cake(10))
-})
-
-const connect_with_redux = connect(
-    map_state_to_props,
-    map_dispatch_to_props,
-)
-export default connect_with_redux(CakeContainer)
+export default HooksCakeContainer
