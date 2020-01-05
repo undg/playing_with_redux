b78f9e9a4a850c6e128378aa3d3846fc135029d9
und3rdg <und3rdg@gmail.com>
2020-01-05 15:17:59 +0000

    This have to be refactored to multiple reducers.

--- index.js
+++ index.js
@@ -8,33 +8,46 @@ const {log_info} = require('./helpers.js')
 const BUY_CAKE = "BUY_CAKE"
 const RESUPPLY_CAKE = "RESUPPLY_CAKE"
 
+const BUY_ICECREAM = "BUY_ICECREAM"
+
+// ACTIONS
 function buy_cake(amount) {
     return {
         type: BUY_CAKE,
-        info: `Buying ${amount} cake`,
+        info: `Buying ${amount} cake(s)`,
         amount: amount,
     }
 }
 const buy_1_cake = {
-        type: BUY_CAKE,
-        info: `Buying one cake`,
-        amount: 1,
-    }
+    type: BUY_CAKE,
+    info: `Buying one cake`,
+    amount: 1,
+}
 
 function resupply_cake(amount) {
     return {
         type: RESUPPLY_CAKE,
-        info: `Delivering ${amount} cakes`,
+        info: `Delivering ${amount} cakes(s)`,
         amount: amount,
     }
 }
 
-
-const initial_state = {
-    num_of_cakes: 10,
+function buy_icecream(amount) {
+    return {
+        type: BUY_ICECREAM,
+        info: `Buying ${amount} icecream(s)`,
+        amount: amount,
+    }
 }
 
-// REDUCER boilerplate
+// STATE 
+const initial_state = {
+    num_of_cakes: 10,
+    num_of_icecream: 20,
+}
+
+// REDUCER
+// boilerplate
 // (previous_state = initial_state, action) => new_state
 
 function reducer(previous_state = initial_state, action) {
@@ -46,11 +59,15 @@ function reducer(previous_state = initial_state, action) {
         case RESUPPLY_CAKE:
             state.num_of_cakes += action.amount
             break
+        case BUY_ICECREAM:
+            state.num_of_icecream -= action.amount
+            break
     }
     log_info(action)
     return state
 }
 
+// USAGE
 const store = create_store(reducer)
 
 console.log('Initial(prev) state:', store.getState(), "\r\n")
@@ -60,6 +77,7 @@ store.dispatch(buy_1_cake)
 store.dispatch(buy_cake(1))
 store.dispatch(resupply_cake(5))
 store.dispatch(buy_cake(6))
+store.dispatch(buy_icecream(9))
 
 unsubscribe()
 
